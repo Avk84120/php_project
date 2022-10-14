a54fbf696e01589241fbb8b95882cbdfd6dc75d1
adityamungse <aditya.mungse@austere.co.in>
2022-10-14 17:14:13 +0530

    first

--- first.php
+++ first.php
@@ -1,60 +1,82 @@
-<body bgcolor="pink">
-    
-
-<form action="first.php" method="post">
-    <h2>Enter your name :</h2>
-    <input type="text" name="name" id="name">
-    <br>
-    <br>
-    
-    <h2>Enter your Email :</h2>
-    <input type="text" name="email" id="email">
-    <br>
-    <br>
-
-    <h2>Enter your Address :</h2>
-    <input type="text" name="address" id="address">
-    <br>
-    <br>
-
-    <h2>Enter your Contact Number :</h2>
-    <input type="text" name="contact" id="contact">
-    <br>
-    <br>
-    <input type="submit" name="submit">
-    
-
-</form>
-</body>
 <?php
-$servername="localhost";
-$username="root";
-$password="root";
+// include 'index.html';
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
 $dbname="crud";
-$conn=new mysqli($servername,$username,$password,$dbname);
-if(!$conn -> connection_error);
-{
-    die("connection_fail...");
-}
+echo "<br>Builoding Connecting...";
 
-//if (isset($_POST['submit'])){
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+echo "<br>After Building cinnection.";
+
+if ($conn->connect_error) 
+{
+    echo "<br>Unable to connect";
+
+    die("Connection failed: " );
+}
+else 
+{
+    echo "<br>Connected.";
+
+    echo "<br>Collecting Variables...";
+    
     $name=$_POST['name'];
     $email=$_POST['email'];
     $address=$_POST['address'];
     $contact=$_POST['contact'];
-
-    $sql="INSERT INTO 'user'('id,'name','email','address','contact') VALUES (NULL,'$name','$email','$address','$contact')";
     
-//if($conn->query($sql) == true)
-if (mysqli_query($conn, $sql))  
+    echo "<br>Builoding 2nd Sql with variables: " . $name . ", " . $email . ", " . $address . ", " . $contact;
+
+    
+    $sql = "INSERT INTO user (name,email,address,contact)    
+    VALUES ('$name', '$email', '$address', '$contact')";
+
+     // values(0,'aniket','avk45@gmail.com','jalagon','8412080720')";
+  
+    if ($conn->query($sql) === TRUE)
     {
-        echo "New Record Add";
-    }
-    else
+        echo "<br>New record created successfully";
+    } 
+    else 
     {
-       echo "Error";
+        echo "<br>Error: " . $sql . "<br>" . $conn->error;
     }
+    echo "<br>Closing Connection...";
+
+    $conn->close(); 
+
+  echo "<br>Connect closed.";   
+  
+}
+/*
+if(isset($_POST["submit"]))
+{
+    echo "<br>Collecting Variables...";
+    
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $address=$_POST['address'];
+    $contact=$_POST['contact'];
+    
+    echo "<br>Builoding 2nd Sql...";
+
+
+    $sql = "INSERT INTO user (id,name,email,address,contact) values(1,'aniket','avk45@gmail.com','jalagon','8412080720')";
+ 
+
+    if ($conn->query($sql) === TRUE)
+    {
+        echo "<br>New record created successfully";
+    } 
+    else 
+    {
+        echo "<br>Error: " . $sql . "<br>" . $conn->error;
+    }
+    echo "<br>Closing Connection...";
 
     $conn->close();
-
+        
+}*/
 ?>
